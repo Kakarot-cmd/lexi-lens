205cce7f517abbe3669a9aed668b55adf71d1730
NJ <beautifulhearts.n@gmail.com>
2026-05-04 08:52:50 +0530

    Feat:Version changes

--- app.config.js
+++ app.config.js
@@ -18,7 +18,7 @@ export default {
   expo: {
     name: "lexi-lens",
     slug: "lexi-lens",
-    version: "1.0.6",
+    version: "1.0.9",
     orientation: "portrait",
     icon: "./assets/icon.png",
     userInterfaceStyle: "light",
